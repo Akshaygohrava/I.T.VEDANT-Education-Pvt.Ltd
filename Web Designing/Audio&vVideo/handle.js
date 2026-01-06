6cc23d4854742f73b8d5aad696919503c0e7b4a1
Akshay Gohrava <akshaygohrava@gmail.com>
2026-01-06 22:34:00 +0530

    javascript update

--- Web Designing/Audio&vVideo/handle.js
+++ Web Designing/Audio&vVideo/handle.js
@@ -1,17 +1,17 @@
-// const password = prompt("Enter Password to access portfolio:");
-// const correctPassword = "admin@1230"; 
+const password = prompt("Enter Password to access portfolio:");
+const correctPassword = "admin@1230"; 
 
-// if (password === null) {
+if (password === null) {
     
-//     alert("Access denied.");
-//     window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
-// } else if (password === correctPassword) {
-//     alert("Welcome to my portfolio!");
-//     // window.location.href = "index.html";
-// } else {
-//     alert("Incorrect password. Redirecting...");
-//     window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
-// }
+    alert("Access denied.");
+    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+} else if (password === correctPassword) {
+    alert("Welcome to my portfolio!");
+    // window.location.href = "index.html";
+} else {
+    alert("Incorrect password. Redirecting...");
+    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
