b26b775873c1b6a010cccf48f8b2ac609484f639
Akshay Gohrava <akshaygohrava@gmail.com>
2026-01-08 21:27:06 +0530

    your name , some videos added

--- Web Designing/Audio&Video/handle.js
+++ Web Designing/Audio&Video/handle.js
@@ -4,13 +4,13 @@ const correctPassword = "admin@1230";
 if (password === null) {
     
     alert("Access denied.");
-    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+    window.location.href = "https://youtu.be/a2GujJZfXpg?si=pOoC3Y_6On4NUzAJ";
 } else if (password === correctPassword) {
     alert("Welcome to my portfolio!");
     // window.location.href = "index.html";
 } else {
     alert("Incorrect password. Redirecting...");
-    window.location.href = "https://youtu.be/EQ94zflNqn4?si=a6BbBwFLBlLrrETQ";
+    window.location.href = "https://youtu.be/a2GujJZfXpg?si=pOoC3Y_6On4NUzAJ";
 }
 
 
