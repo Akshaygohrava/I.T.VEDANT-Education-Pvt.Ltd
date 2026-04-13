735c30ac2bd4df0ac4c26d81f003c7a23a3fb6b7
Akshaygohrava <akshaygohrava@gmail.com>
2026-04-13 08:32:47 +0530

    Java Learning Baiscs, Remastering

---
+++ Java Essentials/Input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);  // Creating Input Object
+    
+    System.out.println("Enter your Age:");
+    int age = sc.nextInt();     // Java Method, to Take inputs
+
+    System.out.println("Your Age is :" + age);
+
+    
+
+          
+    }
+}
---
+++ Java Essentials/Operator2.java
@@ -0,0 +1,19 @@
+public class Operator2 {
+    public static void main(String[] args) {
+        // Relational and Compairison Operator 
+
+        int a = 7;
+        int b = 4;
+
+        System.out.println(a > b); // true , a is greater than b
+        System.out.println(a < b);  // False , a is not greater than b
+
+        System.out.println(a > 7);  //false,  a is not greater , is equal to
+        System.out.println(a >= 7);  // True, 
+        System.out.println(a <= 8);  //false,
+
+        System.out.println(a == 7); // True , a is 7
+        System.out.println(a != 7);  // False , a is 7, not equals to
+        System.out.println(a != 5);  // True  
+    }
+}
