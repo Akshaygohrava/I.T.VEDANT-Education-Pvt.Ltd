ea37c167f82b773f5abef65ed12ea8c190b51be4
AkshayGohrava <akshaygohrava@gmail.com>
2026-07-24 22:42:48 +0530

    inital commit

---
+++ Java Essentials/Java-Learning/springcore/src/main/java/com/study/springcore/App.java
@@ -0,0 +1,32 @@
+package com.study.springcore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello , This Is My First Spring Boot Project");
+        
+        // Annotation     // Bean    // Dependency Injection, feild level, constructor level injection
+        
+        
+        // Types of Annotations 
+        // Componenet annotation creates the beans
+        // bean , method level anotation
+        // Configuration Annotation
+        // autowired . inkect the bean, check in ioc container
+        // Qualifiers 
+        // Service , Creates the bean , marks this class as the business logic 
+        
+        
+        
+        // ORM Object Relational Mapping 
+        
+        // 
+        
+        
+    }
+}
